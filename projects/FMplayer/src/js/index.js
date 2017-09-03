b35ba36c8791c2c67968734146710a1c8d7244fa
mhy-web <dsz9258@qq.com>
2017-09-03 09:57:06 +0800

    FMplayer

--- projects/FMplayer/src/js/index.js
+++ projects/FMplayer/src/js/index.js
@@ -1,2 +1,31 @@
 
-hello
+
+
+var Music = function(){
+	this.init()
+	this.bind()
+}
+
+prot.init = function(){
+	this.login = document.querySelector('#login')
+	this.avatar = document.querySelector('#avatar')
+	this.volume = document.querySelector('volumn')
+
+	this.thumb = document.querySelector('#Thumb-btn')
+	this.collect = document.querySelector('#collect-btn')
+	this.load = document.querySelector('#load-btn')
+	this.review = document.querySelector('#review-btn')
+	this.loop = document.querySelector('#loop')
+
+	this.preplay = document.querySelector('#presplay')
+	this.nextplay = document.querySelector('#nextplay')
+	this.pause = document.querySelector('#pause')
+	this.list = document.querySelector('$listshow')
+
+	this.perc = document.querySelector('#perc')
+
+}
+var prot =  Music.prototype
+prot.init = function(){
+
+}
